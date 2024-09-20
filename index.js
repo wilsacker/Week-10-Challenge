0117761206c821566ae3771ab07969eae7817fba
wilsacker <williamsuttona@gmail.com>
2024-09-20 16:15:26 -0700

    all prompts with console.log answers

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ inquirer.prompt([
         message: 'Enter up to three characters for the logo text:',
         validate: (input) => input.length <= 3 || 'Text must be 3 characters or fewer'
     }
-])
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a color for the text (color keyword or hex code):'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape for the logo:',
+        choices: ['Circle', 'Triangle', 'Square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter a color for the shape (color keyword or hex code):'
+    }
+]).then(answers => {
+    console.log(answers);
+});
