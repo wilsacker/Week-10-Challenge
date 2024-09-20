4b3762ea4773afa05a228e3660ee035953f95464
wilsacker <williamsuttona@gmail.com>
2024-09-20 16:14:07 -0700

    adding prompts

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes');
 
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo text:',
+        validate: (input) => input.length <= 3 || 'Text must be 3 characters or fewer'
+    }
+])
