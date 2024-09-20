12f37ecc3ee8736d8c65fbdc2331435545fcbd72
wilsacker <williamsuttona@gmail.com>
2024-09-20 16:36:39 -0700

    added function to create the logo from answers and create svg file from logo

--- index.js
+++ index.js
@@ -27,7 +27,37 @@ inquirer.prompt([
         message: 'Enter a color for the shape (color keyword or hex code):'
     }
 ]).then(answers => {
-    console.log(answers);
+    // Create the shape based on the user's choice
+    let shape;
+    switch (answers.shape) {
+        case 'Circle':
+            shape = new Circle();
+            break;
+        case 'Triangle':
+            shape = new Triangle();
+            break;
+        case 'Square':
+            shape = new Square();
+            break;
+    }
 
-    
+    // Set the shape's color
+    shape.setColor(answers.shapeColor);
+
+    // Generate the full SVG content
+    const svgContent = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${shape.render()}
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+    </svg>
+    `;
+
+    // Write the SVG content to a file
+    fs.writeFile('logo.svg', svgContent, (err) => {
+        if (err) {
+            console.error('Error writing the file:', err);
+        } else {
+            console.log('Generated logo.svg');
+        }
+    });
 });
